f0cff852005ac2c364e40d1d7cac22c9b2355b2c
froggattsheridan <52541455+froggattsheridan@users.noreply.github.com>
2019-08-15 10:57:29 -0400

    Add files via upload

---
+++ assets/data/bicycleData.ts
@@ -0,0 +1,14 @@
+import { Bicycle } from "../../app/Bicycle";
+
+export const BICYCLEDATA: Bicycle[] = [
+  {
+    model: "x1",
+    color: "red",
+    company: "ccm"
+  },
+  {
+    model: "k2",
+    color: "green",
+    company: "giant"
+  }
+];
